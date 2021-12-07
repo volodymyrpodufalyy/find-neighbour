40f51e13c3df85341c275a3d61f48c93fe9c09d7
Volodymyr Podufalyy <vpodufalyy13@gmail.com>
2021-12-07 16:40:31 +0200

    update production config

--- src/core/socket.js
+++ src/core/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io(window.location.origin.replace('3000','3003')); //на продакшн це потрібно переписати
+const socket = io('https://findneighbour-backend.herokuapp.com/'); //на продакшн це потрібно переписати
 
 export default socket;
